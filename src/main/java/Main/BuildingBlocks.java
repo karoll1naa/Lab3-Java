0704903b4665cf4b3456ec10f1e9139b1301846c
karoll1naa <karollinnajan@gmail.com>
2024-10-24 17:56:09 +0300

    Adding a class of building blocks

---
+++ src/main/java/Main/BuildingBlocks.java
@@ -0,0 +1,23 @@
+package Main;
+
+public class BuildingBlocks {
+    private String name;
+    private String mobilityBlocks; //Тобто, чи можна рухати блок за допомогою поршнів
+    private String locationBlocks; //У якому світі знаходиться
+    public int hardness; //Твердість блоку
+    public int lightLevel; //Рівень світла, який блок випромінює
+    private boolean isTransparent;
+
+    public BuildingBlocks(String name, String mobilityBlocks, String locationBlocks, int hardness, int lightLevel, boolean isTransparent) {
+        this.name = name;
+        this.mobilityBlocks = mobilityBlocks;
+        this.locationBlocks = locationBlocks;
+        this.hardness = hardness;
+        this.lightLevel = lightLevel;
+        this.isTransparent = isTransparent;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/Main/Sorting.java
@@ -0,0 +1,6 @@
+package Main;
+
+public class Sorting {
+    int recordBook = 5543;
+    int C11 = recordBook % 11; //10
+}
